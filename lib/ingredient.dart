bed304b69e1a5a40057edf2e5db8b7263bbddde7
yunjunglab <yunjunglab@gmail.com>
2023-07-02 23:04:50 +0900

    change recipe page to only one scrollable child add TextForm to input amount scale

--- lib/ingredient.dart
+++ lib/ingredient.dart
@@ -2,10 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:bakinglog/page_log.dart';
 import 'package:bakinglog/data.dart';
 
+class AmountChanger extends StatefulWidget {
+  const AmountChanger({required this.amount, required this.refreshCallback, super.key});
+  final String amount;
+  final Function(double) refreshCallback;
+
+  @override
+  _AmountChangerState createState() => _AmountChangerState();
+}
+
+class _AmountChangerState extends State<AmountChanger> {
+  bool _isEditing = false;
+  TextEditingController _textEditingController = TextEditingController();
+  String _amount = '0';
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController();
+    _amount = widget.amount;
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _isEditing = true;
+          _textEditingController.text = _amount; });
+      },
+      child: _isEditing ? TextFormField(
+        controller: _textEditingController,
+        autofocus: true,
+          onFieldSubmitted: (value) {
+            setState(() {
+            _isEditing = false;
+            _amount = value;
+            widget.refreshCallback(2.0); //todo change to ratio and update whole amount
+            });
+        },
+      ) : Text(_amount),
+    );
+  }
+}
+
 class IngredientView extends StatelessWidget {
-  IngredientView({required this.ingredient}) : super(key: ObjectKey(ingredient));
+  IngredientView({required this.ingredient, required this.refreshCallback}) : super(key: ObjectKey(ingredient));
 
   final Ingredient ingredient;
+  final Function(double) refreshCallback;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +80,7 @@ class IngredientView extends StatelessWidget {
         ),
         Container(alignment: Alignment.centerRight,
             width: 50, height: 30,
-            child: Text(ingredient.amount),
+            child: AmountChanger(amount: ingredient.amount, refreshCallback: refreshCallback,),
             color: Theme.of(context).colorScheme.tertiaryContainer
         ),
       ],
--- lib/page_recipe.dart
+++ lib/page_recipe.dart
@@ -3,22 +3,36 @@ import 'package:bakinglog/page_log.dart';
 import 'package:bakinglog/data.dart';
 import 'package:bakinglog/ingredient.dart';
 
-class IngredientList extends StatelessWidget {
-  IngredientList({required this.recipe}) : super(key: ObjectKey(recipe));
-
+class IngredientList extends StatefulWidget {
+  IngredientList({required this.recipe, super.key});
   final Recipe recipe;
+
+  @override
+  State<IngredientList> createState() => _IngredientListState();
+}
+
+class _IngredientListState extends State<IngredientList> {
+  double amountRatio = 1.0;
+  bool refreshFlag = false;
+
+  void refreshChildren(double amountRatio) {
+    setState(() {
+      refreshFlag = !refreshFlag;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
-      shrinkWrap: true,
-      padding: const EdgeInsets.only(top:5.0),
-      itemCount: recipe.ingredients.length,
-      separatorBuilder: (context, index) => const Divider(
-        color: Colors.grey, height: 1.0),
-      itemBuilder: (context, index) {
-        return IngredientView(ingredient: recipe.ingredients[index]);
-      },
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        padding: const EdgeInsets.only(top:5.0),
+        itemCount: widget.recipe.ingredients.length,
+        separatorBuilder: (context, index) => const Divider(
+          color: Colors.grey, height: 1.0),
+        itemBuilder: (context, index) {
+          return IngredientView(ingredient: widget.recipe.ingredients[index], refreshCallback: refreshChildren,);
+        },
     );
   }
 }
@@ -31,15 +45,16 @@ class HowToList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      shrinkWrap: true,
-      padding: const EdgeInsets.all(5),
-      itemCount: recipe.howTo.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Container(alignment: Alignment.centerLeft,
-        height: 30,
-        child: Text('${index+1}. ${recipe.howTo[index]}'));
-     },
-    );
+        shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+        padding: const EdgeInsets.all(0),
+        itemCount: recipe.howTo.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(alignment: Alignment.centerLeft,
+          height: 30,
+          child: Text('   ${index+1}. ${recipe.howTo[index]}'));
+       },
+      );
   }
 }
 
@@ -90,20 +105,24 @@ class _RecipePageState extends State<RecipePage> {
       appBar: AppBar(
           title: Text(widget.recipe.recipeName),
           backgroundColor: Theme.of(context).colorScheme.primaryContainer),
-      body: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Ingredients', style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.primary)),
-            Flexible(child: IngredientList(recipe: widget.recipe)),
-            Divider(height:20, thickness: 10, color: Theme.of(context).colorScheme.primaryContainer),
-            Text('How to', style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.primary)),
-            Flexible(child: HowToList(recipe: widget.recipe)),
-            Divider(height:20, thickness: 10, color: Theme.of(context).colorScheme.primaryContainer),
-            Text('Bake Log', style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.primary)),
-            Flexible(child: LogList(recipe: widget.recipe)),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Divider(height:10, thickness: 0, color: Colors.transparent),
+              Text('  Ingredients', style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.primary)),
+              IngredientList(recipe: widget.recipe),
+              Divider(height:20, thickness: 20, color: Theme.of(context).colorScheme.primaryContainer),
+              Divider(height:5, thickness: 0, color: Colors.transparent),
+              Text('  How to', style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.primary)),
+              Container(child: HowToList(recipe: widget.recipe)),
+              Divider(height:30, thickness: 20, color: Theme.of(context).colorScheme.primaryContainer),
+              Text('  Bake Log', style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.primary)),
+              Container(child: LogList(recipe: widget.recipe)),
+            ],
+          )
         )
       )
     );
