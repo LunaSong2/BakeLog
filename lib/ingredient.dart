f5a7b7b8fdfa055b299870689889bd34c3254a98
yunjunglab <yunjunglab@gmail.com>
2023-07-11 23:21:20 +0900

    Start Edit mode of page_recipe

--- lib/ingredient.dart
+++ lib/ingredient.dart
@@ -15,6 +15,7 @@ class AmountChanger extends StatefulWidget {
 class _AmountChangerState extends State<AmountChanger> {
   bool _isEditing = false;
   String _changedAmount = '';
+
   TextEditingController _textEditingController = TextEditingController();
 
   @override
@@ -50,29 +51,103 @@ class _AmountChangerState extends State<AmountChanger> {
   @override
   Widget build(BuildContext context) {
     _changedAmount = getChangedAmountString();
+
     return GestureDetector(
       onTap: () {
         setState(() {
           _isEditing = true;
-          _textEditingController.text = _changedAmount; });
+          _textEditingController.text = _changedAmount;
+        });
       },
-      child: _isEditing ? TextFormField(
-        controller: _textEditingController,
-        autofocus: true,
+      child: Container(
+        width: 60, height: 30,
+        color: Theme.of(context).colorScheme.tertiaryContainer,
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(left:5, right:5),
+        child: _isEditing ? TextFormField(
+          controller: _textEditingController,
+          autofocus: true,
+          textAlign: TextAlign.right,
+
+          onTapOutside: (value) {
+            setState(() {
+              _isEditing = false;
+            });
+          },
           onFieldSubmitted: (value) {
             setState(() {
               _isEditing = false;
               widget.refreshCallback(double.parse(value)/widget.amount);
               _changedAmount = getChangedAmountString();
-            });},
-      ) : Text(_changedAmount)
+            });
+          },
+        ) : Text(_changedAmount) ),
       );
   }
 }
 
-class IngredientView extends StatelessWidget {
-  IngredientView({required this.ingredient, required this.amountRatio, required this.refreshCallback}) : super(key: ObjectKey(ingredient));
+class IngredientTextField extends StatefulWidget {
+  IngredientTextField({required this.isEdit, required this.width, required this.ingredient, required this.type, required this.style, required this.alignment, super.key});
 
+  final bool isEdit;
+  final double width;
+  final Ingredient ingredient;
+  final int type;
+  final TextStyle? style;
+  final AlignmentGeometry alignment;
+
+  @override
+  State<IngredientTextField> createState() => _IngredientTextFieldState();
+}
+
+class _IngredientTextFieldState extends State<IngredientTextField> {
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.type == 0) {
+      textEditingController.text = widget.ingredient.name;
+    } else if (widget.type == 1) {
+      textEditingController.text = widget.ingredient.amount;
+    } else {
+      textEditingController.text = widget.ingredient.unit;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.isEdit ? Container(
+      alignment: widget.alignment,
+      width: widget.width, height: 30,
+      decoration: BoxDecoration(
+          border: Border.all(
+              color: Theme.of(context).colorScheme.tertiaryContainer,
+              width: 2.0)),
+      child: TextField(
+          controller: textEditingController,
+          onChanged: (value) { //todo check performance
+            if (widget.type == 0) {
+              widget.ingredient.name = value;
+            } else if (widget.type == 1) {
+              widget.ingredient.amount = value;
+            } else {
+              widget.ingredient.unit = value;
+            }
+          }
+      )
+    )
+    : Container(alignment: widget.alignment,
+      width: widget.width, height: 30,
+      child: Text(textEditingController.text, style: widget.style),
+    );
+  }
+}
+
+class IngredientView extends StatelessWidget {
+  IngredientView({required this.isEdit, required this.ingredient, required this.amountRatio, required this.refreshCallback}) : super(key: ObjectKey(ingredient));
+
+  final bool isEdit;
   final Ingredient ingredient;
   final Function(double) refreshCallback;
   final double amountRatio;
@@ -85,23 +160,29 @@ class IngredientView extends StatelessWidget {
           width: 30, height: 30,
           child: const Icon(Icons.arrow_right),
         ),
-        Container(alignment: Alignment.centerLeft,
-          width: 100, height: 30,
-          child: Text(ingredient.name, style: Theme.of(context).textTheme.bodyLarge),
-        ),
-        Container(alignment: Alignment.centerRight,
-          width: 50, height: 30,
-          child: Text(ingredient.amount, style: Theme.of(context).textTheme.titleLarge),
-        ),
-        Container(alignment: Alignment.centerLeft,
-          width: 50, height: 30,
-          child: Text(' ' + ingredient.unit),
-        ),
-        Container(alignment: Alignment.centerRight,
-            width: 60, height: 30,
-            child: AmountChanger(amount: double.parse(ingredient.amount), amountRatio: amountRatio, refreshCallback: refreshCallback,),
-            color: Theme.of(context).colorScheme.tertiaryContainer
-        ),
+        IngredientTextField(
+          isEdit: isEdit,
+          width: 100,
+          ingredient: ingredient,
+          type: 0, //todo replace enum
+          style: Theme.of(context).textTheme.bodyLarge,
+          alignment: Alignment.centerLeft),
+        IngredientTextField(
+          isEdit: isEdit,
+          width: 50,
+          ingredient: ingredient,
+          type: 1,
+          style: Theme.of(context).textTheme.titleLarge,
+          alignment: Alignment.centerRight),
+        Container(width: 10, height:30),
+        IngredientTextField(
+          isEdit: isEdit,
+          width: 50,
+          ingredient: ingredient,
+          type: 2,
+          style: Theme.of(context).textTheme.bodyLarge,
+          alignment: Alignment.centerLeft),
+        isEdit ? Container() : AmountChanger(amount: double.parse(ingredient.amount), amountRatio: amountRatio, refreshCallback: refreshCallback,),
       ],
     );
   }
--- lib/page_recipe.dart
+++ lib/page_recipe.dart
@@ -4,8 +4,9 @@ import 'package:bakinglog/data.dart';
 import 'package:bakinglog/ingredient.dart';
 
 class IngredientList extends StatefulWidget {
-  IngredientList({required this.recipe, super.key});
+  IngredientList({required this.recipe, required this.isEdit, super.key});
   final Recipe recipe;
+  final bool isEdit;
 
   @override
   State<IngredientList> createState() => _IngredientListState();
@@ -31,30 +32,58 @@ class _IngredientListState extends State<IngredientList> {
         separatorBuilder: (context, index) => const Divider(
           color: Colors.grey, height: 1.0),
         itemBuilder: (context, index) {
-          return IngredientView(ingredient: widget.recipe.ingredients[index], amountRatio: amountRatio, refreshCallback: refreshChildren,);
+          return IngredientView(isEdit: widget.isEdit, ingredient: widget.recipe.ingredients[index], amountRatio: amountRatio, refreshCallback: refreshChildren,);
         },
     );
   }
 }
 
-class HowToList extends StatelessWidget {
-  HowToList({required this.recipe}) : super(key: ObjectKey(recipe));
+class HowToList extends StatefulWidget {
+  HowToList({required this.recipe, required this.isEdit}) : super(key: ObjectKey(recipe));
 
   final Recipe recipe;
+  final bool isEdit;
+
+  @override
+  _HowToListState createState() => _HowToListState();
+}
+
+class _HowToListState extends State<HowToList> {
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    textEditingController.text = widget.recipe.howTo[0];
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return widget.isEdit ? Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Theme.of(context).colorScheme.tertiaryContainer,
+          width: 2.0)),
+        child: TextField(
+          controller: textEditingController,
+          maxLines: null,
+          keyboardType: TextInputType.multiline,
+          textAlign: TextAlign.left,
+          onChanged: (value) { //todo check performance
+            widget.recipe.howTo[0] = value;
+          }
+        ))
+      : ListView.builder(
         shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
+        physics: NeverScrollableScrollPhysics(),
         padding: const EdgeInsets.all(0),
-        itemCount: recipe.howTo.length,
+        itemCount: widget.recipe.howTo.length,
         itemBuilder: (BuildContext context, int index) {
           return Container(alignment: Alignment.centerLeft,
-          height: 30,
-          child: Text('   ${index+1}. ${recipe.howTo[index]}'));
+          //height: 30,
+          child: Text('${widget.recipe.howTo[index]}'));
        },
-      );
+    );
   }
 }
 
@@ -93,18 +122,27 @@ class RecipePage extends StatefulWidget {
 }
 
 class _RecipePageState extends State<RecipePage> {
-  int count = 0;
+  bool isEdit = false;
 
-  void _addCount() {
-    setState(() {count++;});
+  void turnEditMode() {
+    setState(() {
+      isEdit = !isEdit;
+      print("turnEditMode $isEdit");
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(widget.recipe.recipeName),
-          backgroundColor: Theme.of(context).colorScheme.primaryContainer),
+          actions: [
+            IconButton(
+            icon: isEdit ? const Icon(Icons.done) : const Icon(Icons.edit),
+            onPressed: () {turnEditMode();}),
+            IconButton(icon: const Icon(Icons.delete), onPressed: (){}),
+          ],
+        title: Text(widget.recipe.recipeName),
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer),
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(0.0),
@@ -113,11 +151,11 @@ class _RecipePageState extends State<RecipePage> {
             children: <Widget>[
               Divider(height:10, thickness: 0, color: Colors.transparent),
               Text('  Ingredients', style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.primary)),
-              IngredientList(recipe: widget.recipe),
+              IngredientList(recipe: widget.recipe, isEdit: isEdit),
               Divider(height:20, thickness: 20, color: Theme.of(context).colorScheme.primaryContainer),
               Divider(height:5, thickness: 0, color: Colors.transparent),
               Text('  How to', style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.primary)),
-              Container(child: HowToList(recipe: widget.recipe)),
+              Container(child: HowToList(recipe: widget.recipe, isEdit: isEdit,)),
               Divider(height:30, thickness: 20, color: Theme.of(context).colorScheme.primaryContainer),
               Text('  Bake Log', style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.primary)),
               Container(child: LogList(recipe: widget.recipe)),
